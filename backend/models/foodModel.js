d1f460468df98d26966e25c03f9efc413963e394
Huy Tran <trandoquanghuy123@gmail.com>
2024-05-15 06:18:02 -0700

    create foodModel

---
+++ backend/models/foodModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+// Mô tả thuộc tính có trong food
+const foodSchema = new mongoose.Schema({
+    name: { 
+        type: String,
+        required: true
+    },
+
+    description: { 
+        type: String, 
+        required: true
+    },
+
+    price: { 
+        type: Number,
+        required: true
+    },
+
+    image: { 
+        type: String, 
+        required: true
+    },
+    
+    category: {
+        type: String,
+        required: true
+    }
+
+})
+
+// Tạo mô hình (models) dựa trên schema
+const foodModel = mongoose.models.food || mongoose.model('food' , foodSchema) // property 1 : name of foodModel
+
+export default foodModel
